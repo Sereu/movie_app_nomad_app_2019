b416f618c9b583ad06d253e75765996527f5f57e
Serue <jisakn93@naver.com>
2019-09-25 23:00:37 +0900

    Updates

---
+++ precache-manifest.ca91677caecdfb8d037e7532997937d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "680ca3720fbdc9824c88e7bbbcfe240d",
+    "url": "/movie_app_nomad_app_2019/index.html"
+  },
+  {
+    "revision": "3d60038744e61d1d185c",
+    "url": "/movie_app_nomad_app_2019/static/css/main.3426f69d.chunk.css"
+  },
+  {
+    "revision": "10d1abe839fdfe0b522e",
+    "url": "/movie_app_nomad_app_2019/static/js/2.5fb175e6.chunk.js"
+  },
+  {
+    "revision": "3d60038744e61d1d185c",
+    "url": "/movie_app_nomad_app_2019/static/js/main.f067a262.chunk.js"
+  },
+  {
+    "revision": "90403306aed8d5042322",
+    "url": "/movie_app_nomad_app_2019/static/js/runtime~main.74f18aef.js"
+  }
+]);
